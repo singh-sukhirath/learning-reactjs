98c7ed8f8a299d4d38b01c28a19f082b480118fc
singh-sukhirath <learning.sukhirath@gmail.com>
2023-09-04 19:52:56 +0530

    Code Cleanup

--- src/components/Investments/Investments.jsx
+++ src/components/Investments/Investments.jsx
@@ -19,11 +19,10 @@ const Investment = () => {
     let yearlySavings = +userInput["currentSavings"];
     let totalInterest = 0;
 
-    const yearlyContribution = +userInput["yearlyContribution"]; // as mentioned: feel free to change the shape...
+    const yearlyContribution = +userInput["yearlyContribution"]; 
     const expectedReturn = +userInput["expectedReturn"] / 100;
     const duration = +userInput["investmentDuration"];
 
-    // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
       currentSavings += yearlyInterest + yearlyContribution;
@@ -31,7 +30,6 @@ const Investment = () => {
       totalInterest += yearlyInterest;
 
       yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
         key: i,
         year: i + 1,
         yearlyInterest: formatter.format(yearlyInterest),
@@ -39,8 +37,7 @@ const Investment = () => {
         totalInterest: formatter.format(totalInterest),
         investedCapital: formatter.format(yearlySavings),
       });
-    }
-    // do something with yearlyData ...
+    } 
 
     if (yearlyData.length > 0) {
       setCalculateData(yearlyData);

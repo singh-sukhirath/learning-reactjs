9215d306bf0fea90228ec25d4fc0c470044c100f
singh-sukhirath <learning.sukhirath@gmail.com>
2023-09-04 19:55:19 +0530

    Code cleanup based on Udemy Video

--- src/components/InvestmentForm/InvestmentForm.jsx
+++ src/components/InvestmentForm/InvestmentForm.jsx
@@ -1,108 +1,118 @@
 import { useState } from "react";
 import InvestmentFormInput from "./InvestmentFormInput";
-import styles from './InvestmentForm.module.css';
+import styles from "./InvestmentForm.module.css";
+
+
+const intialUserInput = {
+  currentSavings: "",
+  yearlyContribution: "",
+  expectedReturn: "",
+  investmentDuration: "",
+}
 
 const InvestmentForm = (props) => {
-  
-  const [userInput, setUserInput] = useState({
-    currentSavings : '',
-    yearlyContribution : '',
-    expectedReturn : '',
-    investmentDuration : ''
-  });
-  
-  const inputChangeHandler = (event) => {
+  const [userInput, setUserInput] = useState(intialUserInput);
 
-    if(event.target.id === 'current-savings')
-    {
-      setUserInput((prevValue) => {
-        return {
-          ...prevValue,
-          currentSavings: +event.target.value
-        }
-      })
-    }
-    else if(event.target.id === 'yearly-contribution')
-    {
-      setUserInput((prevValue) => {
-        return {
-          ...prevValue,
-          yearlyContribution: +event.target.value
-        }
-      })
-    }
-    else if(event.target.id === 'expected-return')
-    {
-      setUserInput((prevValue) => {
-        return {
-          ...prevValue,
-          expectedReturn: +event.target.value
-        }
-      })
-    }
-    else if(event.target.id === 'duration')
-    {
-      setUserInput((prevValue) => {
-        return {
-          ...prevValue,
-          investmentDuration: +event.target.value
-        }
-      })
-    }
-  }
+  const inputChangeHandler = (input, value) => {
+    setUserInput((prevValue) => {
+      return {
+        ...prevValue,
+        [input]: value,
+      };
+    });
+  };
+
+  // const inputChangeHandler = (event) => {
+
+  //   if(event.target.id === 'current-savings')
+  //   {
+  //     setUserInput((prevValue) => {
+  //       return {
+  //         ...prevValue,
+  //         currentSavings: +event.target.value
+  //       }
+  //     })
+  //   }
+  //   else if(event.target.id === 'yearly-contribution')
+  //   {
+  //     setUserInput((prevValue) => {
+  //       return {
+  //         ...prevValue,
+  //         yearlyContribution: +event.target.value
+  //       }
+  //     })
+  //   }
+  //   else if(event.target.id === 'expected-return')
+  //   {
+  //     setUserInput((prevValue) => {
+  //       return {
+  //         ...prevValue,
+  //         expectedReturn: +event.target.value
+  //       }
+  //     })
+  //   }
+  //   else if(event.target.id === 'duration')
+  //   {
+  //     setUserInput((prevValue) => {
+  //       return {
+  //         ...prevValue,
+  //         investmentDuration: +event.target.value
+  //       }
+  //     })
+  //   }
+  // }
 
   const formResetHandler = () => {
-    setUserInput({
-      currentSavings : '',
-      yearlyContribution : '',
-      expectedReturn : '',
-      investmentDuration : ''
-    });
+    setUserInput(intialUserInput);
 
-    props.onFormSubmit('');
-  }
+    props.onFormSubmit(intialUserInput);
+  };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
     props.onFormSubmit(userInput);
-  }
+  };
 
   return (
     <form className={`${styles.form}`} onSubmit={formSubmitHandler}>
-      <div className={styles['input-group']}>
+      <div className={styles["input-group"]}>
         <InvestmentFormInput
           inputId="current-savings"
           inputType="number"
           inputText="Current Savings ($)"
-          inputValue= {userInput.currentSavings}
-          onInputChange={inputChangeHandler}
+          inputValue={userInput.currentSavings}
+          onInputChange={(event) => inputChangeHandler('currentSavings', event.target.value)}
         />
         <InvestmentFormInput
           inputId="yearly-contribution"
           inputType="number"
           inputText="Yearly Savings ($)"
-          inputValue= {userInput.yearlyContribution}
-          onInputChange={inputChangeHandler}
+          inputValue={userInput.yearlyContribution}
+          onInputChange={(event) => inputChangeHandler('yearlyContribution', event.target.value)}
         />
       </div>
-      <div className={styles['input-group']}>
+      <div className={styles["input-group"]}>
         <InvestmentFormInput
           inputId="expected-return"
           inputType="number"
           inputText="Expected Interest (%, per year)"
-          inputValue= {userInput.expectedReturn}
-          onInputChange={inputChangeHandler}
+          inputValue={userInput.expectedReturn}
+          onInputChange={(event) => inputChangeHandler('expectedReturn', event.target.value)}
         />
         <InvestmentFormInput
           inputId="duration"
           inputType="number"
           inputText="Investment Duration (years)"
-          inputValue= {userInput.investmentDuration}
-          onInputChange={inputChangeHandler}
+          inputValue={userInput.investmentDuration}
+          onInputChange={(event) => inputChangeHandler('investmentDuration', event.target.value)}
         />
       </div>
       <p className={styles.actions}>
-        <button type="reset" className={styles.buttonAlt} onClick={formResetHandler}>
+        <button
+          type="reset"
+          className={styles.buttonAlt}
+          onClick={formResetHandler}
+        >
           Reset
         </button>
         <button type="submit" className={`${styles.button}`}>
